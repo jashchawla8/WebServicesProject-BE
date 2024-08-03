f65faeb8e51128ae4ca0e9afa461e6790ca1291d
jaschawla8 <jashchawla@Jashs-MBP.lan>
2024-08-02 22:50:26 -0500

    "Dashboard and List Projects"

--- app.py
+++ app.py
@@ -90,5 +90,82 @@ def create_project():
     return jsonify({'message': 'Project created successfully', 'project_id': project_id}), 201
 
 
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    data = request.json
+    user_id = data.get('userId')
+
+
+    # Fetch the user's organization id
+    user = db.users.find_one({"userId": user_id})
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    org_id = user['orgId']
+    user_projects = user['projectId']
+
+    # Fetch all users in the same organization
+    team = list(db.users.find({"orgId": org_id}, {"_id": 0, "userId": 1, "firstName": 1,"lastName":1, "role": 1}))
+
+    # Format team data
+    formatted_team = [{"userid": member["userId"], "name": member["firstName"] +" " + member["lastName"], "role": member["role"]} for member in
+                      team]
+
+    # Fetch hardware utilization for the organization
+
+    projects = list(db.projects.find({"orgId": org_id, "projectId": {"$in": user_projects}}, {"_id": 0, "projectId": 1, "name": 1, "hwSet1": 1, "hwSet2": 1}))
+
+    print(projects)
+
+
+    total_org_hw1_utilisation = sum(int(project['hwSet1']) for project in projects)
+    total_org_hw2_utilisation = sum(int(project['hwSet2']) for project in projects)
+
+    # Fetch project details
+
+    project_details = [{
+        "pid": project['projectId'],
+        "name": project['name'],
+        "hwset1": int(project['hwSet1']),
+        "hwset2": int(project['hwSet2'])
+    } for project in projects]
+
+    response = {
+        "team": formatted_team,
+        "totalOrgHW1Utilisation": total_org_hw1_utilisation,
+        "totalOrgHW2Utilisation": total_org_hw2_utilisation,
+        "projects": project_details
+    }
+
+    return jsonify(response)
+
+@app.route('/dashboard/project', methods=['POST'])
+def dashboard():
+    data = request.json
+    user_id = data.get('userId')
+
+    user = db.users.find_one({"userId": user_id})
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    org_id = user['orgId']
+    user_projects = user['projectId']
+    projects = list(db.projects.find({"orgId": org_id, "projectId": {"$in": user_projects}}, {"_id": 0, "projectId": 1, "name": 1, "hwSet1": 1, "hwSet2": 1, "date_created": 1}))
+
+    project_details = [{
+        "date_created" : project['date_crated'],
+        "pid": project['projectId'],
+        "name": project['name'],
+        "hwset1": int(project['hwSet1']),
+        "hwset2": int(project['hwSet2'])
+    } for project in projects]
+
+    response = {"projects" : project_details}
+
+    return jsonify(response)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
