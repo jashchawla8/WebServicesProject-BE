7891e44e707936bb6ff21d58d7bac7f713f909d3
harshsharm24 <harshsharma2413@gmail.com>
2024-08-06 20:34:54 -0500

    Removed dead code

--- organizations.py
+++ organizations.py
@@ -1,10 +1,3 @@
-def check_project_does_not_exist(db, projectId):
-    try:
-        project = db.projects.find_one({"projectId": projectId})
-        return project is None
-    except Exception as e:
-        return {'Error occurred: ' + str(e)}
-    
 def check_org_does_not_exist(db, orgId):
     try:
         org = db.organizations.find_one({"orgId": orgId})
